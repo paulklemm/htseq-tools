e859006e2eaa3915b3a669decad964287ebd8a77
Paul Klemm <paul.klemm@sf.mpg.de>
2018-04-05 12:31:23 +0200

    Update hisat_genome_index function

--- hisat_genome_index_2.sh
+++ hisat_genome_index_2.sh
@@ -2,6 +2,8 @@
 
 # Example usage: 
 # ./hisat_genome_index.sh --ensemblbuild 91 --species "Homo_sapiens" --genome GRCh38 --output "../genome" --logs "../slurm_logs"
+# ./hisat_genome_index.sh --genome ftp://ftp.ensembl.org/pub/release-91/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP6.dna.toplevel.fa.gz
+# ./hisat_genome_index_2.sh --genome "ftp://ftp.ensembl.org/pub/release-91/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP6.dna.toplevel.fa.gz" --output "../genome" --logs "../slurm_logs"
 
 # Parameter read-in from https://stackoverflow.com/a/14203146/2274058
 POSITIONAL=()
@@ -10,18 +12,8 @@ do
 key="$1"
 
 case $key in
-    -e|--ensemblbuild)
-    ENSEMBL_BUILD="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -s|--species)
-    SPECIES="$2"
-    shift # past argument
-    shift # past value
-    ;;
     -g|--genome)
-    GENOME="$2"
+    GENOME_URL="$2"
     shift # past argument
     shift # past value
     ;;
@@ -47,12 +39,19 @@ LOGS=$(readlink -f ${LOGS})/
 mkdir $OUTPUT
 mkdir $LOGS
 
-echo ENSEMBL BUILD = "${ENSEMBL_BUILD}"
-echo SPECIES       = "${SPECIES}"
-echo GENOME        = "${GENOME}"
-echo OUTPUT        = "${OUTPUT}"
-echo LOGS          = "${LOGS}"
+ENSEMBL_BUILD=$(echo "${GENOME_URL}" | sed 's/.*release-\([^/]*\).*/\1/')
+SPECIES=$(echo "${GENOME_URL}" | sed 's/.*dna\/\([^\.]*\).*/\1/')
+GENOME=$(echo "${GENOME_URL}" | sed 's/.*dna\/[^\.]*\.\([^\.]*\).*/\1/')
+GENOME_COMPLETE_NAME=$(echo "${GENOME_URL}" | sed 's/.*\///')
+GENOME_COMPLETE_NAME_WITHOUT_EXTENSION=$(echo "${GENOME_COMPLETE_NAME}" | sed 's/\.fa.gz//')
 
+echo ENSEMBL BUILD                          = "${ENSEMBL_BUILD}"
+echo SPECIES                                = "${SPECIES}"
+echo GENOME                                 = "${GENOME}"
+echo OUTPUT                                 = "${OUTPUT}"
+echo LOGS                                   = "${LOGS}"
+echo GENOME_COMPLETE_NAME                   = "${GENOME_COMPLETE_NAME}"
+echo GENOME_COMPLETE_NAME_WITHOUT_EXTENSION = "${GENOME_COMPLETE_NAME_WITHOUT_EXTENSION}"
 
 # Create Splicesites
 # Docker image Bioinf
@@ -80,8 +79,10 @@ source /beegfs/scratch/bruening_scratch/pklemm/shifter/home/.bashrc
 module load hisat
 cd ${OUTPUT}
 # Download FA and GTFs
-wget ftp://ftp.ensembl.org/pub/release-${ENSEMBL_BUILD}/fasta/${SPECIES,,}/dna/${SPECIES}.${GENOME}.dna.primary_assembly.fa.gz
-gunzip ${SPECIES}.${GENOME}.dna.primary_assembly.fa.gz
+# wget ftp://ftp.ensembl.org/pub/release-${ENSEMBL_BUILD}/fasta/${SPECIES,,}/dna/${SPECIES}.${GENOME}.dna.primary_assembly.fa.gz
+# gunzip ${SPECIES}.${GENOME}.dna.primary_assembly.fa.gz
+wget ${GENOME_URL}
+gunzip ${GENOME_COMPLETE_NAME}
 # Download transcriptome
 wget ftp://ftp.ensembl.org/pub/release-${ENSEMBL_BUILD}/gtf/${SPECIES,,}/${SPECIES}.${GENOME}.${ENSEMBL_BUILD}.gtf.gz
 gunzip ${SPECIES}.${GENOME}.${ENSEMBL_BUILD}.gtf.gz
@@ -89,6 +90,7 @@ gunzip ${SPECIES}.${GENOME}.${ENSEMBL_BUILD}.gtf.gz
 # Make index
 hisat2_extract_splice_sites.py ${SPECIES}.${GENOME}.${ENSEMBL_BUILD}.gtf > splicesites.tsv
 hisat2_extract_exons.py ${SPECIES}.${GENOME}.${ENSEMBL_BUILD}.gtf > exons.tsv
-hisat2-build -p 32 --ss splicesites.tsv --exon exons.tsv ${SPECIES}.${GENOME}.dna.primary_assembly.fa ${SPECIES}.${GENOME}.dna.primary_assembly
+# hisat2-build -p 32 --ss splicesites.tsv --exon exons.tsv ${SPECIES}.${GENOME}.dna.primary_assembly.fa ${SPECIES}.${GENOME}.dna.primary_assembly
+hisat2-build -p 32 --ss splicesites.tsv --exon exons.tsv $(echo "${GENOME_COMPLETE_NAME_WITHOUT_EXTENSION}.fa") ${GENOME_COMPLETE_NAME_WITHOUT_EXTENSION}
 SHI
 EOF
